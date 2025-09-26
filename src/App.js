940424d667a86ecf7dca2f5ce4e709cc4d0e9921
Ngnminhtam <vhminhtam1@gmail.com>
2025-09-26 15:06:15 +0700

    install & config Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from './components/Layout';
+import { Fragment } from 'react';
+
 function App() {
     return (
-        <div className="App">
-            <h1>ReactJS</h1>
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) Layout = route.layout;
+                        //Fragment: Component chỉ để chứa, không sinh ra thẻ thật trong DOM
+                        else if (route.layout === null) Layout = Fragment;
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
